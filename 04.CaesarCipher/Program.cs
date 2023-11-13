696d7067e03dd76d4cc3f8c2e2943bbba0d81a16
Ivan Kratchanov <ivankra@gmail.com>
2023-11-13 23:02:23 +0200

    Add files via upload

---
+++ 04.CaesarCipher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine();
+
+        string encryptedText = EncryptText(inputText);
+
+        Console.WriteLine($"{encryptedText}");
+
+        Console.ReadLine();
+    }
+
+    static string EncryptText(string text)
+    {
+        StringBuilder encryptedText = new StringBuilder();
+
+        foreach (char character in text)
+        {
+            char encryptedChar = (char)(((int)character + 3));
+
+            encryptedText.Append(encryptedChar);
+        }
+
+        return encryptedText.ToString();
+    }
+}
---
+++ 06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = Console.ReadLine();
+
+        string result = CollapseDuplicateCharacters(inputString);
+
+        Console.WriteLine($"{result}");
+
+        Console.ReadLine(); 
+    }
+
+    static string CollapseDuplicateCharacters(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+
+        StringBuilder result = new StringBuilder();
+        char currentChar = input[0];
+        result.Append(currentChar);
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            if (input[i] != currentChar)
+            {
+                currentChar = input[i];
+                result.Append(currentChar);
+            }
+        }
+
+        return result.ToString();
+    }
+}
